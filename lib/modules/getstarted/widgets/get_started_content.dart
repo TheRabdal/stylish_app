7beb3934b55dc23fce0634ad1a17fe9fe85cc367
TheRabdal <kautsarrabdal@gmail.com>
2025-12-30 05:10:29 +0700

    feat: add Get Started content widget and its associated asset

--- lib/modules/getstarted/widgets/get_started_content.dart
+++ lib/modules/getstarted/widgets/get_started_content.dart
@@ -5,49 +5,52 @@ class GetStartedContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-          colors: [Colors.black.withOpacity(0.7), Colors.transparent],
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.5,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/reactangle.png'),
+            fit: BoxFit.fill,
+          ),
         ),
-      ),
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                "You want \nAuthentic, here \nyou go!",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.montserrat(
-                  color: Colors.white,
-                  fontSize: 34,
-                  fontWeight: FontWeight.bold,
-                  height: 1.2,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "You want \nAuthentic, here \nyou go!",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                    height: 1.2,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Find it here, buy it now!",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.montserrat(
-                  color: const Color(0xFFF2F2F2),
-                  fontSize: 14,
+                const SizedBox(height: 10),
+                Text(
+                  "Find it here, buy it now!",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    color: const Color(0xFFF2F2F2),
+                    fontSize: 14,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 40),
-              Button(
-                text: "Get Started",
-                onPressed: () {
-                  // Navigate to Home or relevant page
-                  debugPrint("Get Started Pressed");
-                },
-              ),
-              const SizedBox(height: 30),
-            ],
+                const SizedBox(height: 40),
+                Button(
+                  text: "Get Started",
+                  onPressed: () {
+                    // Navigate to Home or relevant page
+                    debugPrint("Get Started Pressed");
+                  },
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
           ),
         ),
       ),
