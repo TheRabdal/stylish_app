b51280b3d4b807b81c20b4dce34648b53dd28533
TheRabdal <kautsarrabdal@gmail.com>
2025-12-30 05:01:09 +0700

    feat: add initial Get Started page with background and content

--- lib/modules/getstarted/screens/get_started_page.dart
+++ lib/modules/getstarted/screens/get_started_page.dart
@@ -1,10 +1,15 @@
 import 'package:stylish_app/packages/packages.dart';
 
-class GetStartedPage extends StatelessWidget {
+class GetStartedPage extends StatefulWidget {
   const GetStartedPage({super.key});
 
   static const String route = 'GetStartedPage';
 
+  @override
+  State<GetStartedPage> createState() => _GetStartedPageState();
+}
+
+class _GetStartedPageState extends State<GetStartedPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
