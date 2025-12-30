d6c4b94281f7b436c0893dacac6f0e9345c84d00
TheRabdal <kautsarrabdal@gmail.com>
2025-12-30 15:52:06 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //const ProductDetailScreen(), 
+      //home: ProductDetailScreen(), 
       initialRoute: SplashPage.route,
       routes: appRoutes,
     );
