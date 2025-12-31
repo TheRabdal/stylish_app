5dbe0f4f3de203028c0ecd6bf7166c56c94870b6
TheRabdal <kautsarrabdal@gmail.com>
2025-12-31 16:18:21 +0700

    mengubah ukuran shape

--- lib/modules/getstarted/widgets/get_started_content.dart
+++ lib/modules/getstarted/widgets/get_started_content.dart
@@ -8,48 +8,49 @@ class GetStartedContent extends StatelessWidget {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
-        height: MediaQuery.of(context).size.height * 0.5,
+        width: 394,
+        height: 362,
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/shapetransparant.png'),
             fit: BoxFit.fill,
           ),
         ),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  "You want \nAuthentic, here \nyou go!",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.montserrat(
-                    color: Colors.white,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                    height: 1.2,
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                "You want \nAuthentic, here \nyou go!",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  color: Colors.white,
+                  fontSize: 34,
+                  fontWeight: FontWeight.w600,
+                  height: 1.2,
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  "Find it here, buy it now!",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.montserrat(
-                    color: const Color(0xFFF2F2F2),
-                    fontSize: 14,
-                  ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Find it here, buy it now!",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  color: const Color(0xFFF2F2F2),
+                  fontSize: 14,
                 ),
-                const SizedBox(height: 40),
-                Button(
-                  text: "Get Started",
-                  onPressed: () {
-                    Navigator.pushNamed(context, NavigationPage.route);
-                  },
-                ),
-                const SizedBox(height: 30),
-              ],
-            ),
+              ),
+              const SizedBox(height: 40),
+              Button(
+                text: "Get Started",
+                width: 279,
+                height: 55,
+                onPressed: () {
+                  Navigator.pushNamed(context, NavigationPage.route);
+                },
+              ),
+              const SizedBox(height: 30),
+            ],
           ),
         ),
       ),
