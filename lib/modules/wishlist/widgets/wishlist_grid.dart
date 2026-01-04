005e9feae35a577045439d7255a00cff1833fc07
TheRabdal <kautsarrabdal@gmail.com>
2026-01-05 01:04:28 +0700

    benerin widget error

--- lib/modules/wishlist/widgets/wishlist_grid.dart
+++ lib/modules/wishlist/widgets/wishlist_grid.dart
@@ -7,18 +7,16 @@ class WishlistGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: MasonryGridView.count(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        crossAxisCount: 2,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return WishlistProductCard(product: product);
-        },
-      ),
+    return MasonryGridView.count(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      crossAxisCount: 2,
+      mainAxisSpacing: 16,
+      crossAxisSpacing: 16,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return WishlistProductCard(product: product);
+      },
     );
   }
 }
